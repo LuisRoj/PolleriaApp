3e1edb8dca2708ea5e8ab63633db9376617bddda
LuisRoj <luisrojas150520@gmail.com>
2024-06-08 00:18:47 -0500

    Inicializacion

---
+++ src/main/java/pe/com/cibertec/ProyectoPolleriaApp/ProyectoPolleriaAppApplication.java
@@ -0,0 +1,13 @@
+package pe.com.cibertec.ProyectoPolleriaApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoPolleriaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoPolleriaAppApplication.class, args);
+	}
+
+}
